83a6304ade1a1901b87024aa084db574ab2ebf25
bouzuya <m@bouzuya.net>
2016-09-06 14:03:27 +0900

    add work vm

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import * as Vue from 'vue';
 import { CookieStorage } from 'cookie-storage';
 
 type Ratings = {
@@ -18,8 +19,14 @@ const saveRatings = (storage: CookieStorage, ratings: Ratings): void => {
 
 const main = () => {
   const storage = new CookieStorage();
-  const ratingsObj = loadRatings(storage);
-  saveRatings(storage, ratingsObj);
+  const ratings = loadRatings(storage);
+  const week = '2016-W31';
+  const work1 = new Vue({
+    el: `.work-${week}`,
+    data: { rating: typeof ratings[week] === 'undefined' ? 0 : ratings[week] }
+  });
+  console.log(work1);
+  saveRatings(storage, ratings);
 };
 
 if (document.readyState === 'complete') {
