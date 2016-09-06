d9657b8469a3caeefbf0e33701621d5e5a2c0795
bouzuya <m@bouzuya.net>
2016-09-06 09:20:11 +0900

    add load and save ratings

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,32 @@
-import add from './add';
+import { CookieStorage } from 'cookie-storage';
 
-export default function main() {
-  return add(3, 4);
+type Ratings = {
+  [week: string]: number;
+};
+
+const loadRatings = (storage: CookieStorage): Ratings => {
+  const ratingsJsonOrNull = storage.getItem('ratings');
+  const ratingsJson = ratingsJsonOrNull === null
+    ? '{}' : ratingsJsonOrNull;
+  const ratings = <Ratings>JSON.parse(ratingsJson);
+  return ratings;
+};
+
+const saveRatings = (storage: CookieStorage, ratings: Ratings): void => {
+  storage.setItem('ratings', JSON.stringify(ratings));
+};
+
+const main = () => {
+  const storage = new CookieStorage();
+  const ratingsObj = loadRatings(storage);
+  saveRatings(storage, ratingsObj);
+};
+
+if (document.readyState === 'complete') {
+  window.setTimeout(() => main());
+} else {
+  document.addEventListener('DOMContentLoaded', function listener() {
+    document.removeEventListener("DOMContentLoaded", listener);
+    main();
+  });
 }
