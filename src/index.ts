dd38a56ecf48d857d506184312d9c3748061731c
bouzuya <m@bouzuya.net>
2016-09-09 14:21:35 +0900

    add toWorks() and toRatings()

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ import { CookieStorage } from 'cookie-storage';
 type Ratings = {
   [week: string]: number;
 };
+type Work = {
+  rating: number;
+  week: string;
+};
 
 const loadRatings = (storage: CookieStorage): Ratings => {
   const ratingsJsonOrNull = storage.getItem('ratings');
@@ -17,28 +21,47 @@ const saveRatings = (storage: CookieStorage, ratings: Ratings): void => {
   storage.setItem('ratings', JSON.stringify(ratings));
 };
 
+const toWorks = (weeks: string[], ratings: Ratings): Work[] => {
+  const works = weeks.map<Work>((week) => {
+    const work = {
+      rating: typeof ratings[week] === 'undefined' ? 0 : ratings[week],
+      week
+    };
+    return work;
+  });
+  return works;
+};
+
+const toRatings = (works: Work[]): Ratings => {
+  return works.reduce((ratings, work) => {
+    ratings[work.week] = work.rating;
+    return ratings;
+  }, <Ratings>{});
+};
+
 const main = () => {
   const storage = new CookieStorage();
   const ratings = loadRatings(storage);
-  const week = '2016-W31';
-  type WorkComponent = { rating: number; };
-  const data: WorkComponent = {
-    rating: typeof ratings[week] === 'undefined' ? 0 : ratings[week]
-  };
-  const work1 = new Vue({
-    el: `.work-${week}`,
-    data,
-    methods: {
-      decrement(this: WorkComponent): void {
-        this.rating -= 1;
-      },
-      increment(this: WorkComponent): void {
-        this.rating += 1;
+  const weeks = ['2016-W31'];
+  const works = toWorks(weeks, ratings);
+  works.map(({ week, rating }) => {
+    type WorkComponent = { rating: number; };
+    const data: WorkComponent = { rating };
+    const work1 = new Vue({
+      el: `.work-${week}`,
+      data,
+      methods: {
+        decrement(this: WorkComponent): void {
+          this.rating -= 1;
+        },
+        increment(this: WorkComponent): void {
+          this.rating += 1;
+        }
       }
-    }
+    });
+    console.log(work1);
   });
-  console.log(work1);
-  saveRatings(storage, ratings);
+  saveRatings(storage, toRatings(works));
 };
 
 const ready = (callback: Function): void => {
