52d2381fc3be217a6056cb5323b2ce26a7c1df9b
bouzuya <m@bouzuya.net>
2016-10-02 10:03:26 +0900

    2016-W39

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,8 @@ const main = () => {
     '2016-W35',
     '2016-W36',
     '2016-W37',
-    '2016-W38'
+    '2016-W38',
+    '2016-W39'
   ];
   const works = storage.load(weeks);
   const state = { works, isValid: true };
